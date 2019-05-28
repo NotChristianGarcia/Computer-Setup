4846054e9207e8bbe72594591244db26d2ea4bfe
Christian R. Garcia <christian.G21@gmail.com>
2019-05-28 00:11:15 -0500

    Re-did everything, more works now

--- init.sh
+++ init.sh
@@ -1,47 +1,65 @@
 #!/bin/bash
 
+### Check for root
+if [ "$EUID" -ne 0 ]
+  then echo "This script must be ran as root."
+  exit
+fi
+
+### Installing aptitude and base packages
+echo "Installing aptitude and base packages."
 apt install aptitude
 aptitude update
-aptitude upgrade
+aptitude upgrade -y
 
-# Install other stuff
-aptitude install git\
-                 python3\
-                 python3-pip\
-                 chromium-browser\
-                 ipython3\
-                 neovim\
-                 nordvpn\
-                 zsh\
-                 curl\
-                 gnome-tweaks\
-                 numix-gtk-theme\
-                 -y
+aptitude install\
+	git\
+	python3\
+	python3-pip\
+	chromium-browser\
+	neovim\
+	zsh\
+	curl\
+	gnome-tweaks\
+	numix-gtk-theme\
+	-y
 
-pip3 install bs4\
-			 requests\
-			 Flask\
-			 hotqueue\
-			 redis\
-			 selenium
 
-# Set Gnome to Numix theme
-gsettings set org.gnome.desktop.interface gtk-theme "Numix"
-gsettings set org.gnome.desktop.wm.preferences theme "Numix"
+### Initializing neovim
+echo "Initializing neovim."
+cp -a ./configs/nvim ~/.config/nvim
+vim +'PlugInstall --sync' +qa
 
-# Extra installs
-aptitude install wine64\
-                 winetricks
 
-# Setting nvim config
-cp -r ./nvim ~/.config/nvim
-echo :wq | nvim
-
-# Zsh Stuff
-chsh -s /bin/zsh;
+### Zsh
+echo "Installing and initializing Zsh."
+# Make Zsh default
+chsh -s /bin/zsh
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install Powerlevel9k for zsh
-aptitude install fonts-hack-ttf
+# Setting Zsh config
+cp ./configs/zsh/zshrc ~/.zshrc
+
+
+### Set gnome-terminal colors
+echo "Setting gnome-terminal colors."
+export TERMINAL=gnome-terminal-
+./colors.sh
+
+
+### Set gnome-terminal font
+echo "Installing and setting gnome-terminal font."
+mkdir ~/.fonts
+cp ./configs/hack.tff ~/.fonts/
+fc-cache -fv ~/.fonts
+
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")/ use-system-font false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")/ font 'Hack Nerd Font 11'
+
+
+### Set Gnome to Numix theme
+echo "Setting gnome-theme to Numix."
+gsettings set org.gnome.desktop.interface gtk-theme "Numix"
+gsettings set org.gnome.desktop.wm.preferences theme "Numix"
